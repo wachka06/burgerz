b01b96ff960d1090bfdb1ecdff3e7fbb0a279a17
github username <github email address>
2018-07-24 10:16:42 -0400

    Add more styling

--- src/App.js
+++ src/App.js
@@ -5,11 +5,20 @@ import BurgerContainer from './components/BurgerContainer'
 import BurgerDisplay from './components/BurgerDisplay'
 
 class App extends Component {
+  state = {
+    selectedBurger: {
+      id: 8,
+      name: "Green Eggs and Hamburger",
+      category: "Relatable",
+      imgURL: "https://stupiddope.com/wp-content/uploads/2016/05/mcdonalds-green-angry-birds-burger-01.jpg"
+    }
+  }
+
   render() {
     return (
       <div id="App">
         <BurgerContainer />
-        <BurgerDisplay />
+        <BurgerDisplay selectedBurger={this.state.selectedBurger} />
       </div>
     );
   }
--- src/components/BurgerDisplay.js
+++ src/components/BurgerDisplay.js
@@ -3,7 +3,15 @@ import React, { Component } from 'react';
 const BurgerDisplay = (props) => {
   return (
     <div className="BurgerDisplay">
-      This is the display
+      <img src={ props.selectedBurger.imgURL }/>
+      <br/>
+      <h2>{ props.selectedBurger.name }</h2>
+      <br/>
+      <select value={props.selectedBurger.category} onChange={console.log}>
+        <option value="All">All</option>
+        <option value="Relatable">Relatable</option>
+        <option value="Bougie">Bougie</option>
+      </select>
     </div>
   )
 }
